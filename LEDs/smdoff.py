14dd14caaf9081c6e51b53f340f0a6454f02e408
Jeff Cox <jeffcox111@gmail.com>
2019-01-26 20:42:31 +0000

    Added SMD RGB LED testing scripts.

---
+++ LEDs/smdoff.py
@@ -0,0 +1,17 @@
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+green = 18
+red = 23
+blue = 24
+
+
+GPIO.setup(green, GPIO.OUT)
+GPIO.setup(red, GPIO.OUT)
+GPIO.setup(blue, GPIO.OUT)
+
+GPIO.output(green, 0)
+GPIO.output(red, 0)
+GPIO.output(blue, 0)
