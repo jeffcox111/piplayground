b68ec8c2875495ce3ce26bf36a64fdcebc79588f
Jeff Cox <jeffcox111@gmail.com>
2022-10-17 11:24:19 -0500

    Switch to PWM to fix color shifting

--- LEDs/pico_rgbcolors.py
+++ LEDs/pico_rgbcolors.py
@@ -1,27 +1,33 @@
-from machine import Pin
+from machine import Pin, PWM
 import utime
 
-red = Pin(16, Pin.OUT)
-green = Pin(18, Pin.OUT)
-blue = Pin(20, Pin.OUT)
+# red = Pin(16, Pin.OUT)
+# green = Pin(18, Pin.OUT)
+# blue = Pin(20, Pin.OUT)
 
 led = Pin(25, Pin.OUT)
 led.value(1)
 
+freq = 100
+
+pwm_red=PWM(Pin(16))
+pwm_green = PWM(Pin(18))
+pwm_blue = PWM(Pin(20))
+
 while True:
 
     for x in range (1,101):
-        green.value(x)
+        pwm_green.duty_u16(x)
         utime.sleep(0.025)
     for x in range (1,101):
-        red.value(101-x)
+        pwm_red.duty_u16(101-x)
         utime.sleep(0.025)
     for x in range(1,101):
-        green.value(101-x)
-        blue.value(x)
+        pwm_green.duty_u16(101-x)
+        pwm_blue.duty_u16(x)
         utime.sleep(0.025)
     for x in range(1,101):
-        red.value(x)
+        pwm_red.duty_u16(x)
         utime.sleep(0.025)
         
 
