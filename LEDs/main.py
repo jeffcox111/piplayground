ff3f0bc51231c1a8cef985bac59d7161d82d25ce
Jeff Cox <jeffcox111@gmail.com>
2022-10-17 13:54:35 -0500

    Fixed brightness issue

--- LEDs/main.py
+++ LEDs/main.py
@@ -1,33 +1,31 @@
 from machine import Pin, PWM
 import utime
 
-# red = Pin(16, Pin.OUT)
-# green = Pin(18, Pin.OUT)
-# blue = Pin(20, Pin.OUT)
-
 led = Pin(25, Pin.OUT)
 led.value(1)
 
-freq = 100
-
-pwm_red=PWM(Pin(16))
+pwm_red = PWM(Pin(16))
 pwm_green = PWM(Pin(18))
 pwm_blue = PWM(Pin(20))
+
+pwm_red.freq(100)
+pwm_green.freq(100)
+pwm_blue.freq(100)
 
 while True:
 
     for x in range (1,101):
-        pwm_green.duty_u16(x)
+        pwm_green.duty_u16(int(x * 0.01 * 32768))
         utime.sleep(0.025)
     for x in range (1,101):
-        pwm_red.duty_u16(101-x)
+        pwm_red.duty_u16(int(32768 - x * 0.01 * 32768))
         utime.sleep(0.025)
     for x in range(1,101):
-        pwm_green.duty_u16(101-x)
-        pwm_blue.duty_u16(x)
+        pwm_green.duty_u16(int(32768 - x * 0.01 * 32768))
+        pwm_blue.duty_u16(int(x * 0.01 * 32768))
         utime.sleep(0.025)
     for x in range(1,101):
-        pwm_red.duty_u16(x)
+        pwm_red.duty_u16(int(x * 0.01 * 32768))
         utime.sleep(0.025)
         
 
